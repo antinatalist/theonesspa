bb7f2e3a65ab15ee7090323433643bd5770a8edc
arthur <arthurwei123@gmail.com>
2022-08-12 18:06:58 -0400

    added contact form with smtpjs

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ document.querySelector(".disableRightClick").addEventListener('contextmenu',  ev
 
 
 //document.querySelector(".myVideo").play();
+
+function sendEmail(){
+
+  Email.send({
+    SecureToken : "0e6130fe-9489-4343-8d10-1638675a23ab",
+    To : 'waymarketingltd@gmail.com',
+    From : 'waymarketingltd@gmail.com',
+    Subject: document.querySelector("#subject").value,
+    Body : 
+    `
+    Name: ${document.querySelector("#name").value}<br><br>
+
+    Email: ${document.querySelector("#email").value}<br><br>
+
+    Phone: ${document.querySelector("#phone").value}<br><br>
+
+    Message: ${document.querySelector("#message").value}
+    `
+  }).then(
+    alert("Thank you. Your message has been sent successfully")
+  );
+}
